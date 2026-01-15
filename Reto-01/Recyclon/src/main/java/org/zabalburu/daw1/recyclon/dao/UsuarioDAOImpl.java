875ec25b6ceec09bfc78b0e5ff2698661d30ef24
DavidDuqueM <david.duque.molina@zabalburu.org>
2026-01-15 13:56:07 +0100

    Correcciones de Iñigo (Falta mirar)

--- Reto-01/Recyclon/src/main/java/org/zabalburu/daw1/recyclon/dao/UsuarioDAOImpl.java
+++ Reto-01/Recyclon/src/main/java/org/zabalburu/daw1/recyclon/dao/UsuarioDAOImpl.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package org.zabalburu.daw1.gestionempleados.dao;
+package org.zabalburu.daw1.recyclon.dao;
 
 import java.util.ArrayList;
 import java.util.List;
